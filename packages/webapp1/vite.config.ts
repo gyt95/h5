4a232e947fa54200234a109a505aefdf426df04d
kwan <gyt95@163.com>
2022-10-23 12:27:33 +0800

    build: add server port

--- packages/webapp1/vite.config.ts
+++ packages/webapp1/vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
-import ViteBaseConfig from '../../vite.base.config'
+// import ViteBaseConfig from '../../vite.base.config'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  ...ViteBaseConfig,
+  // ...ViteBaseConfig,
   server: {
-    port: 8020,
+    port: 8010,
+    host: '0.0.0.0',
   },
+  plugins: [vue()],
 })
--- packages/webapp2/vite.config.ts
+++ packages/webapp2/vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
-import ViteBaseConfig from '../../vite.base.config'
+// import ViteBaseConfig from '../../vite.base.config'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  ...ViteBaseConfig,
+  // ...ViteBaseConfig,
   server: {
-    port: 8010,
+    port: 8020,
+    host: '0.0.0.0',
   },
+  plugins: [vue()],
 })
