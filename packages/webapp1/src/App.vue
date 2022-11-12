6cec83a2b44974fc1fe3a6564299a4eeab779126
kwan <gyt95@163.com>
2022-11-12 10:23:07 +0800

    feat(webapp1): call utils function in core

--- packages/webapp1/src/App.vue
+++ packages/webapp1/src/App.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
+import { checkPhoneNumber } from '@h5/core'
 import { defineAsyncComponent, onMounted } from 'vue'
 import { userInfoUrl } from './api'
 const RemoteButton = defineAsyncComponent(() => import('@webapp2/Button'))
 const RemoteFooter = defineAsyncComponent(() => import('@webapp2/Footer'))
+
+console.log('检查 10086 是否为手机号：', checkPhoneNumber('10086')) // false
+console.log('检查 19866004816 是否为手机号：', checkPhoneNumber('19866004816')) // true
 
 onMounted(async () => {
   await userInfoUrl()
